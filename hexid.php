80796eeaf959762c1040c7794080027dee7d1007
Komil <icecool@users.noreply.github.com>
2019-09-24 12:41:37 +0500

    Create hexid.php
    something like this for hex id generation

---
+++ hexid.php
@@ -0,0 +1,46 @@
+<?php
+
+function GetRandomHex($amount=100,$range=[100000000,200000000],$filter=true)
+{
+    $h=[];
+    $i=0;
+    //$j=0;
+    while($i<$amount)
+    {
+        //$j++;
+        $x = random_int($range[0],$range[1]);
+        $s = strtoupper(dechex($x));
+        $pos = strpos($s,'6');
+        if($filter && ($pos !== false || strlen($s)!=7)) {continue;}
+        if(!isset($h[$s]))
+        {
+            $h[] = $s;
+        }
+        $i++;
+    }
+    //echo 'Total iterations: '.$j.'<br>';
+    return $h;
+}
+
+function csv2json($path='data.csv')
+{
+    $p=[];
+    if(($handle=fopen($path,"r"))!==FALSE)
+    {
+        while(($data=fgetcsv($handle,1000,","))!==FALSE)
+        {
+            $p[]=[trim($data[0]),trim($data[1])];
+        }
+        fclose($handle);
+    }
+    echo json_encode($p);
+}
+
+//csv2json();
+
+$hex=GetRandomHex();
+for ($i=0,$c=count($hex); $i < $c; $i++) { 
+    echo $hex[$i].'<br>';
+}
+
+?>
